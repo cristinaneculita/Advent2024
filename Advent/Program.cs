374700fcefd9d642910a54a7f06245d35dbb2993
Cristina Neculita <cristina.neculita@scania.com>
2024-12-05 07:13:22 +0100

    ziua5 partea2

--- Advent/Program.cs
+++ Advent/Program.cs
@@ -6,11 +6,13 @@ using System.Globalization;
 using System.Linq.Expressions;
 using System.Numerics;
 using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
 
 string[] lines = File.ReadAllLines("input.txt");
 var l = lines.Length;
 var sum = 0;
+var sum2 = 0;
 var rules = new List<Rule>();
 var prints = new List<List<int>>();
 //citire date
@@ -45,49 +47,145 @@ for (int i = 0; i < l; i++)
 
 foreach (var print in prints)
 {
-    var corect = true;
-    foreach (var el in print)
-    {
-        var ruleForBefore = rules.Where(r => r.Before == el);
-        var indcurent = print.IndexOf(el);
-        foreach (var rule in ruleForBefore)
-        {
-            var afternum = rule.After;
-            var indr = print.IndexOf(afternum);
-            if (indr!=-1 && indr < indcurent)
-            {
-                corect = false;
-                break;
-            }
-        }
-        if (corect == false)
-            break;
-        var ruleForAfter = rules.Where(r => r.After == el);
-        foreach (var rule in ruleForAfter)
-        {
-            var beforeNr = rule.Before;
-            var indr = print.IndexOf(beforeNr);
-            if (indr!=-1 && indr > indcurent)
-            {
-                corect = false;
-                break;
-            }
-        }
-    }
+
+    var corect = Corect(print, rules);
 
     if (corect)
     {
         var mid = print.Count / 2;
         sum += print[mid];
     }
+    else
+    {
+        var resultList = new List<int>();
+        int first=0;
+        while (print.Any())
+        {
+            foreach (var p in print)
+            {
+                //let's put p first
+                var newList = new List<int>();
+                newList.Add(p);
+                foreach (var pp in print)
+                {
+                    if (pp != p)
+                    {
+                        newList.Add(pp);
+                    }
+                }
 
+                if (CorectOnlyMe(newList, rules, p))
+                {
+                    resultList.Add(p);
+                    first = p;
+                    break;
+                }
+            }
 
+            print.Remove(first);
+        }
+        Console.WriteLine();
+
+        foreach (var i in resultList)
+        {
+            Console.Write($"{i} ");
+        }
+
+        var mid = resultList.Count / 2;
+        sum2 += resultList[mid];
+    }
 }
 
 
+int mostRulesBeforeLeastAfter(List<int> print, List<Rule> rules)
+{
+    var mostrules = int.MinValue;
+    int val=0;
+    foreach (var el in print)
+    {
+        var b = rules.Count(r => r.Before == el && print.Contains(r.After));
+        if (b > mostrules)
+        {
+            mostrules = b;
+            val = el;
+        }
+    }
+
+    return val;
+}
 
 Console.WriteLine(sum);
+Console.WriteLine(sum2);
 
+bool Corect(List<int> ints, List<Rule> list)
+{
+    var b = true;
+    foreach (var el in ints)
+    {
+        var ruleForBefore = list.Where(r => r.Before == el);
+        var indcurent = ints.IndexOf(el);
+        foreach (var rule in ruleForBefore)
+        {
+            var afternum = rule.After;
+            var indr = ints.IndexOf(afternum);
+            if (indr!=-1 && indr < indcurent)
+            {
+                b = false;
+                break;
+            }
+        }
+        if (b == false)
+            break;
+        var ruleForAfter = list.Where(r => r.After == el);
+        foreach (var rule in ruleForAfter)
+        {
+            var beforeNr = rule.Before;
+            var indr = ints.IndexOf(beforeNr);
+            if (indr!=-1 && indr > indcurent)
+            {
+                b = false;
+                break;
+            }
+        }
+    }
+
+    return b;
+}
+
+bool CorectOnlyMe(List<int> lis, List<Rule> rr, int r)
+{
+    var b = true;
+    var el = r;
+        var ruleForBefore = rr.Where(r => r.Before == el);
+        var indcurent = lis.IndexOf(el);
+        foreach (var rule in ruleForBefore)
+        {
+            var afternum = rule.After;
+            var indr = lis.IndexOf(afternum);
+            if (indr != -1 && indr < indcurent)
+            {
+                b = false;
+                break;
+            }
+        }
+
+        if (b == false)
+            return false;
+        var ruleForAfter = rr.Where(r => r.After == el);
+        foreach (var rule in ruleForAfter)
+        {
+            var beforeNr = rule.Before;
+            var indr = lis.IndexOf(beforeNr);
+            if (indr != -1 && indr > indcurent)
+            {
+                b = false;
+                break;
+            }
+        }
+    
+
+    return b;
+}
 
 
 class Rule
