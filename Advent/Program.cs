95b6cfdebf2a8e809d251834efda3430f6b9f6a0
Cristina Neculita <cristina.neculita@scania.com>
2024-12-09 07:16:17 +0100

    ziua 9 partea1

--- Advent/Program.cs
+++ Advent/Program.cs
@@ -12,127 +12,74 @@ using System.Runtime.Serialization;
 
 string[] lines = File.ReadAllLines("input.txt");
 var l = lines.Length;
-char[][] m = new char[l][];
-bool[][] anti = new bool[l][];
-var antenas = new List<char>();
 long sum = 0;
+int[] val = new int[lines[0].Length];
 //citire date
 for (int i = 0; i < l; i++)
 {
-    m[i] = lines[i].ToCharArray();
-    anti[i] = new bool[l];
+    for (int j = 0; j < lines[i].Length; j++)
+    {
+        val[j] = int.Parse(lines[i][j].ToString());
+    }
 }
 
+long indmic = 0;
+long indmare = 0;
+long indmicdr = val.Length-1;
+long indmaredr = val.Length / 2;
+long pusiDinDr = 0;
+long many = HowManyInBigArray();
+
+long HowManyInBigArray()
+{
+    long s = 0;
+    for (int i = 0; i < val.Length; i+=2)
+    {
+        s += val[i];
+    }
+    return s;
+}
 
 //prelucrare date
-for (int i = 0; i < l; i++)
+for (int i = 0; i < val.Length; i++)
 {
-    for (int j = 0; j < l; j++)
+    if (i % 2 == 0)
     {
-        if (m[i][j] != '.')
+        for (int j = 0; j < val[i]; j++)
         {
-            if (!antenas.Contains(m[i][j]))
-                antenas.Add(m[i][j]);
+            sum += indmare * indmic;
+            indmare++;
+            if (indmare == many)
+                break;
         }
-
-        anti[i][j] = false;
+        indmic++;
     }
-}
-
-foreach (var antena in antenas)
-{
-    List<Point> allOcc = GetAllOcc(antena);
-
-
-
-    foreach (var occ1 in allOcc)
+    else
     {
-        foreach (var occ2 in allOcc)
+        for (int j = 0; j < val[i]; j++)
         {
-            if (occ1 != occ2)
+            //var rm = FindRightmost();
+            sum += indmaredr * indmare;
+            pusiDinDr++;
+            if (pusiDinDr == val[indmicdr])
             {
-                
-                FindAllAntinodes(anti, occ1, occ2);
-
-
+                pusiDinDr = 0;
+                indmicdr -= 2;
+                indmaredr -= 1;
             }
+
+            indmare++;
+          
         }
     }
 
-}
-
-for (int i = 0; i < l; i++)
-{
-    for (int j = 0; j < l; j++)
-    {
-        if (anti[i][j])
-            sum++;
-    }
+    if (indmare == many)
+        break;
 }
 
 Console.WriteLine(sum);
-
-void FindAllAntinodes(bool[][] anti, Point occ1, Point occ2)
+long FindRightmost()
 {
-    var diffx = occ1.X - occ2.X;
-    var diffy = occ1.Y - occ2.Y;
-    var anti1x = occ1.X + diffx;
-    var anti1y = occ1.Y + diffy;
-    if (InLimits(anti1x) && InLimits(anti1y))
-        anti[anti1x][anti1y] = true;
-    var anti2x = occ2.X - diffx;
-    var anti2y = occ2.Y - diffy;
-    if (InLimits(anti1x) && InLimits(anti1y))
-        anti[anti1x][anti1y] = true;
+    return indmaredr;
 
-}
-bool InLimits(int coord)
-{
-    if (coord >= 0 && coord < l)
-    { return true; }
-
-    return false;
-}
-List<Point> GetAllOcc(char c)
-{
-    var result = new List<Point>();
-    for (int i = 0; i < l; i++)
-    {
-        for (int j = 0; j < l; j++)
-        {
-            if (m[i][j] == c)
-                result.Add(new Point(i, j));
-        }
-    }
-    return result;
-}
-public class Point
-{
-    public int X { get; set; }
-    public int Y { get; set; }
-
-    public Point(int x, int y)
-    {
-        X = x;
-        Y = y;
-    }
-
-    public override bool Equals(object obj)
-    {
-        var item = obj as Point;
-
-        if (item == null)
-        {
-            return false;
-        }
-
-        if (item.X == this.X && item.Y == this.Y)
-            return true;
-        return false;
-    }
-
-    public override int GetHashCode()
-    {
-        return this.X.GetHashCode();
-    }
 }
