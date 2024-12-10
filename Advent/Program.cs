1c6401a062b9d17a14c280117129b20a69e6c7ad
Cristina Neculita <cristina.neculita@scania.com>
2024-12-10 06:48:42 +0100

    ziua 10

--- Advent/Program.cs
+++ Advent/Program.cs
@@ -13,161 +13,91 @@ using System.Runtime.Serialization;
 
 string[] lines = File.ReadAllLines("input.txt");
 var l = lines.Length;
-long sum = 0;
-int[] val = new int[lines[0].Length];
+int[][] m = new int[l][];
+int sum = 0;
+int[][] nines = new int[l][];
 //citire date
 for (int i = 0; i < l; i++)
 {
-    for (int j = 0; j < lines[i].Length; j++)
+    var v = lines[i].ToCharArray();
+    m[i]=new int[l];
+    nines[i] = new int[l];
+    for (int j = 0; j < l; j++)
     {
-        val[j] = int.Parse(lines[i][j].ToString());
+        m[i][j] = v[j] - '0';
+        nines[i][j] = 0;
     }
+
+    
 }
 
-long indmic = 0;
-long indmare = 0;
-long indmicdr = val.Length-1;
-long indmaredr = val.Length / 2;
-long pusiDinDr = 0;
-long many = HowManyInBigArray();
-long manyTotal = HowManyTotal();
-long[] valori = new long[manyTotal];
-
-for (int i = 0; i < val.Length; i++)
+//procesare date
+for (int i = 0; i < l; i++)
 {
-    if (i % 2 == 0)
+    for (int j = 0; j < l; j++)
     {
-        for (int j = 0; j < val[i]; j++)
+        if (m[i][j] == 0)
         {
-            valori[indmare] = i/2;
-            indmare++;
+            for (int k = 0; k < l; k++)
+            {
+                for (int n = 0; n < l; n++)
+                {
+                    nines[k][n] = 0;
+                }
+            }
+            SearchPath(i, j, 0);
         }
+    }
+}
+Console.WriteLine(sum);
 
-        
+void SearchPath(int i, int j, int pas)
+{
+    if (pas == 9)
+    {
+     //   if (nines[i][j] == 0)
+        {
+            sum++;
+            nines[i][j] = 1;
+        }
+        return;
     }
     else
     {
-        for (int j = 0; j < val[i]; j++)
-        {
-            valori[indmare] = -1;
-            indmare++;
-        }
+        if(vecinsus(i,j)==pas+1)
+            SearchPath(i-1,j,pas+1);
+        if (vecindr(i, j) == pas + 1)
+            SearchPath(i, j+1, pas + 1);
+        if (vecinjos(i, j) == pas + 1)
+            SearchPath(i+1, j, pas+1);
+        if (vecinstg(i, j) == pas + 1)
+            SearchPath(i, j-1, pas + 1);
     }
 }
 
-indmaredr = valori.Length - 1;
-indmicdr = val.Length - 1;
-var toMove = val[indmicdr];
-while (true)
+int vecinsus(int i, int j)
 {
-    var moved = Move();
-    indmaredr -= val[indmicdr - 1];
-    if (!moved)
-        indmaredr -= toMove;
-    indmicdr -=2;
-    toMove = val[indmicdr];
-    if (indmicdr == 0)
-        break;
+    if (i > 0)
+        return m[i-1][j];
+    return -1;
 }
 
-for (int i = 0; i < manyTotal; i++)
+int vecindr(int i, int j)
 {
-    if (valori[i] != -1)
-        sum += i * valori[i];
+    if (j <l-1)
+        return m[i][j+1];
+    return -1;
 }
 
-bool Move()
+int vecinjos(int i, int j)
 {
-    var laRand = 0;
-    for (int i = 0; i < indmaredr; i++)
-    {
-        if (valori[i] == -1)
-        {
-            laRand++;
-            if (laRand == toMove)
-            {
-                for (int j = 0; j<toMove; j++)
-                {
-                    valori[indmaredr] = -1;
-                    indmaredr--;
-                    valori[i] = indmicdr/2;
-                    i--;
-                }
-
-                return true;
-            }
-        }
-        else
-        {
-            laRand = 0;
-        }
-    }
-
-    return false;
+    if (i <l-1)
+        return m[i+1][j];
+    return -1;
 }
-Console.WriteLine(sum);
-
-
-long HowManyTotal()
+int vecinstg(int i, int j)
 {
-    long s = 0;
-    for (int i = 0; i < val.Length; i += 1)
-    {
-        s += val[i];
-    }
-    return s;
-}
-
-long HowManyInBigArray()
-{
-    long s = 0;
-    for (int i = 0; i < val.Length; i+=2)
-    {
-        s += val[i];
-    }
-    return s;
-}
-
-//prelucrare date
-//for (int i = 0; i < val.Length; i++)
-//{
-//    if (i % 2 == 0)
-//    {
-//        for (int j = 0; j < val[i]; j++)
-//        {
-//            sum += indmare * indmic;
-//            indmare++;
-//            if (indmare == many)
-//                break;
-//        }
-//        indmic++;
-//    }
-//    else
-//    {
-//        for (int j = 0; j < val[i]; j++)
-//        {
-//            //var rm = FindRightmost();
-//            sum += indmaredr * indmare;
-//            pusiDinDr++;
-//            if (pusiDinDr == val[indmicdr])
-//            {
-//                pusiDinDr = 0;
-//                indmicdr -= 2;
-//                indmaredr -= 1;
-//            }
-
-//            indmare++;
-          
-//        }
-//    }
-
-//    if (indmare == many)
-//        break;
-//}
-
-Console.WriteLine(sum);
-long FindRightmost()
-{
-    return indmaredr;
-
+    if (j > 0)
+        return m[i][j-1];
+    return -1;
 }
