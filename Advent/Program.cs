838cf95077c4e335d595d9ba7a059ad274755ffd
Cristina Neculita <cristina.neculita@scania.com>
2024-12-11 08:08:51 +0100

    ziua12 partea1

--- Advent/Program.cs
+++ Advent/Program.cs
@@ -12,92 +12,51 @@ using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
 
 string[] lines = File.ReadAllLines("input.txt");
-var l = lines.Length;
-int[][] m = new int[l][];
-int sum = 0;
-int[][] nines = new int[l][];
-//citire date
-for (int i = 0; i < l; i++)
-{
-    var v = lines[i].ToCharArray();
-    m[i]=new int[l];
-    nines[i] = new int[l];
-    for (int j = 0; j < l; j++)
-    {
-        m[i][j] = v[j] - '0';
-        nines[i][j] = 0;
-    }
 
-    
+
+int sum = 0;
+List<long> lst = new List<long>();
+//citire date
+var x = lines[0].Split(' ');
+foreach (var s in x)
+{
+    lst.Add(long.Parse(s));
 }
 
 //procesare date
-for (int i = 0; i < l; i++)
+for (int i = 0; i < 25; i++)
 {
-    for (int j = 0; j < l; j++)
+
+    Console.WriteLine();
+    lst = ProcesareLista();
+    Console.Write(i +"->" +lst.Count+"--->");
+    //foreach (var l in lst)
+    //{
+    //    Console.Write(l + " ");
+    //}
+}
+
+
+Console.WriteLine(lst.Count);
+List<long> ProcesareLista()
+{
+    var newlist = new List<long>();
+    foreach (var l in lst)
     {
-        if (m[i][j] == 0)
+        if(l==0)
+            newlist.Add(1);
+        else if (l.ToString().Length % 2 == 0)
         {
-            for (int k = 0; k < l; k++)
-            {
-                for (int n = 0; n < l; n++)
-                {
-                    nines[k][n] = 0;
-                }
-            }
-            SearchPath(i, j, 0);
+            var strl = l.ToString();
+            var firsthalf = strl.Substring(0, strl.Length / 2);
+            var secondhalf = strl.Substring(strl.Length / 2);
+            newlist.Add(long.Parse(firsthalf));
+            newlist.Add(long.Parse(secondhalf));
+        }
+        else
+        {
+            newlist.Add(l*2024);
         }
     }
-}
-Console.WriteLine(sum);
-
-void SearchPath(int i, int j, int pas)
-{
-    if (pas == 9)
-    {
-     //   if (nines[i][j] == 0)
-        {
-            sum++;
-            nines[i][j] = 1;
-        }
-        return;
-    }
-    else
-    {
-        if(vecinsus(i,j)==pas+1)
-            SearchPath(i-1,j,pas+1);
-        if (vecindr(i, j) == pas + 1)
-            SearchPath(i, j+1, pas + 1);
-        if (vecinjos(i, j) == pas + 1)
-            SearchPath(i+1, j, pas+1);
-        if (vecinstg(i, j) == pas + 1)
-            SearchPath(i, j-1, pas + 1);
-    }
-}
-
-int vecinsus(int i, int j)
-{
-    if (i > 0)
-        return m[i-1][j];
-    return -1;
-}
-
-int vecindr(int i, int j)
-{
-    if (j <l-1)
-        return m[i][j+1];
-    return -1;
-}
-
-int vecinjos(int i, int j)
-{
-    if (i <l-1)
-        return m[i+1][j];
-    return -1;
-}
-int vecinstg(int i, int j)
-{
-    if (j > 0)
-        return m[i][j-1];
-    return -1;
+    return newlist;
 }
