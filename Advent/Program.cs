756950d8b53266ad7184bf299cb01d19d4c3fb64
Cristina Neculita <cristina.neculita@scania.com>
2024-12-15 10:12:49 +0100

    ziua 15 partea1

--- Advent/Program.cs
+++ Advent/Program.cs
@@ -12,133 +12,204 @@ using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
+using System.Xml;
 using System.Xml.Schema;
 
-    string[] lines = File.ReadAllLines("input.txt");
+string[] lines = File.ReadAllLines("input.txt");
 var l = lines.Length;
 //Hashtable memo = new Hashtable();
-var xmax = 101;
-var ymax = 103;
-//var xmax = 11;
-//var ymax = 7;
+char[] instr = new char[1];
 long sum = 0;
-var robots = new List<Robot>();
-bool[][] imagine = new bool[xmax][];
-for (int i = 0; i < xmax; i++)
-{
-    imagine[i] = new bool[ymax];
-    for (int j = 0; j < ymax; j++)
-    {
-        imagine[i][j] = false;
-    }
-
-}
+char[][] map = new char[lines[0].Length][];
 //citire date
 for (int i = 0; i < l; i++)
 {
-    var x = lines[i].Split(',');
-    var t1 = int.Parse(x[0].Substring(2));
-    var y = x[1].Split(" ");
-    var t2 = int.Parse(y[0]);
-    var t3 = int.Parse(y[1].Substring(2));
-    var t4 = int.Parse(x[2]);
-    Robot r = new Robot(t1, t2, t3, t4);
-    robots.Add(r);
+    if (lines[i].Length>0 && lines[i][0] == '#')
+    {
+        map[i] = lines[i].ToCharArray();
+    }
+    else if (!string.IsNullOrEmpty(lines[i]))
+    {
+        instr = lines[i].ToCharArray();
+    }
 }
+
+var ll = lines[0].Length;
+int ri=0, rj=0;
 //procesare date
-
-for (int i = 0; i < 10000; i++)
+for (int i = 0; i < ll; i++)
 {
-    Console.WriteLine(i);
-    foreach (var robot in robots)
+    for (int j = 0; j < ll; j++)
     {
-        Step(robot);
-    }
-
-    if (((i - 62) % 103 == 0) || ((i - 81) % 101 == 0))
-    {
-        Write(robots);
-        Console.ReadKey();
-    }
-}
-   
-
-
-var sfert1 = 0;
-var sfert2 = 0;
-var sfert3 = 0;
-var sfert4 = 0;
-var medx = xmax / 2;
-var medy = ymax / 2;
-
-foreach (var robot in robots)
-{
-    if (robot.Posx < medx && robot.Posy < medy)
-        sfert1++;
-    if (robot.Posx > medx && robot.Posy < medy)
-        sfert2++;
-    if (robot.Posx < medx && robot.Posy > medy)
-        sfert3++;
-    if (robot.Posx > medx && robot.Posy > medy)
-        sfert4++;
-}
-
-Console.WriteLine(sfert1 * sfert2 * sfert3 * sfert4);
-void Step(Robot robot)
-{
-    robot.Posx = (robot.Posx + robot.Velx) % xmax;
-    if (robot.Posx < 0)
-        robot.Posx = xmax + robot.Posx;
-    robot.Posy = (robot.Posy + robot.Vely) % ymax;
-    if (robot.Posy < 0)
-        robot.Posy = ymax + robot.Posy;
-}
-void Write(List<Robot> list)
-{
-    for (int i = 0; i < ymax; i++)
-    {
-        for (int j = 0; j < xmax; j++)
+        if (map[i][j] == '@')
         {
-            imagine[j][i] = false;
+            ri = i;
+            rj = j;
         }
     }
-
-    foreach (var robot in list)
-    {
-        imagine[robot.Posx][robot.Posy] = true;
-    }
-
-    for (int i = 0; i < ymax; i++)
-    {
-       // Console.Write(i);
-        for (int j = 0; j < xmax; j++)
-        {
-            if (imagine[j][i])
-                Console.Write('#');
-            else
-                Console.Write('.');
-        }
-
-        Console.WriteLine();
-    }
 }
-public class Robot
+
+for (int i = 0; i < instr.Length; i++)
 {
-    public int Posx { get; set; }
-    public int Posy { get; set; }
-    public int Velx { get; set; }
-    public int Vely { get; set; }
-
-    public Robot()
+    switch (instr[i])
     {
-
+        case '^':
+            MoveUp();
+            break;
+        case '>':
+            MoveRight();
+            break;
+        case '<':
+            MoveLeft();
+            break;
+        case 'v':
+            MoveDown();
+            break;
     }
 
-    public Robot(int posx, int posy, int velx, int vely)
+    //Console.WriteLine(instr[i]);
+    //for (int ii = 0; ii < ll; ii++)
+    //{
+    //    for (int jj = 0; jj < ll; jj++)
+    //    {
+    //        Console.Write(map[ii][jj]);
+    //    }
+    //    Console.WriteLine();
+    //}
+}
+
+
+
+
+for (int i = 0; i < ll; i++)
+{
+    for (int j = 0; j < ll; j++)
     {
-        Posx = posx;
-        Posy = posy;
-        Velx = velx;
-        Vely = vely;
+        if (map[i][j] == 'O')
+            sum += 100 * i + j;
     }
 }
+
+
+Console.WriteLine(sum);
+void MoveDown()
+{
+    switch (map[ri + 1][rj])
+    {
+        case '.':
+            map[ri][rj] = '.';
+            ri = ri + 1;
+            map[ri][rj] = '@';
+            break;
+        case '#':
+            break;
+        case 'O':
+            MoveColumnDown();   
+            break;
+    }
+}
+void MoveLeft()
+{
+    switch (map[ri][rj-1])
+    {
+        case '.':
+            map[ri][rj] = '.';
+            rj = rj - 1;
+            map[ri][rj] = '@';
+            break;
+        case '#':
+            break;
+        case 'O':
+            MoveColumnLeft();
+            break;
+    }
+}
+void MoveRight()
+{
+    switch (map[ri][rj+1])
+    {
+        case '.':
+            map[ri][rj] = '.';
+            rj = rj + 1;
+            map[ri][rj] = '@';
+            break;
+        case '#':
+            break;
+        case 'O':
+            MoveColumnRight();
+            break;
+    }
+}
+void MoveUp()
+{
+    switch (map[ri - 1][rj])
+    {
+        case '.':
+            map[ri][rj] = '.';
+            ri = ri - 1;
+            map[ri][rj] = '@';
+            break;
+        case '#':
+            break;
+        case 'O':
+            MoveColumnUp();
+            break;
+    }
+}
+
+void MoveColumnUp()
+{
+    int c = 1;
+    while (map[ri - c][rj] == 'O')
+        c++;
+    if (map[ri - c][rj] == '.')
+    {
+        map[ri - c][rj] = 'O';
+        map[ri][rj] = '.';
+        ri = ri - 1;
+        map[ri][rj] = '@';
+    }
+}
+
+void MoveColumnDown()
+{
+    int c = 1;
+    while (map[ri + c][rj] == 'O')
+        c++;
+    if (map[ri + c][rj] == '.')
+    {
+        map[ri + c][rj] = 'O';
+        map[ri][rj] = '.';
+        ri = ri + 1;
+        map[ri][rj] = '@';
+    }
+}
+
+void MoveColumnLeft()
+{
+    int c = 1;
+    while (map[ri][rj-c] == 'O')
+        c++;
+    if (map[ri][rj-c] == '.')
+    {
+        map[ri][rj-c] = 'O';
+        map[ri][rj] = '.';
+        rj = rj - 1;
+        map[ri][rj] = '@';
+    }
+}
+
+void MoveColumnRight()
+{
+    int c = 1;
+    while (map[ri][rj + c] == 'O')
+        c++;
+    if (map[ri][rj + c] == '.')
+    {
+        map[ri][rj + c] = 'O';
+        map[ri][rj] = '.';
+        rj = rj + 1;
+        map[ri][rj] = '@';
+    }
+}
+Console.WriteLine(sum);
