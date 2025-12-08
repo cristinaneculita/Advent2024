88f13920dab2c4c8b738ceaefe1ac0c3f7916f46
Cristina Neculita <cristina.neculita@scania.com>
2025-12-08 12:18:02 +0100

    2025 8_1

--- Advent/Program.cs
+++ Advent/Program.cs
@@ -14,6 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
+using System.Security.Cryptography;
 using System.Xml;
 using System.Xml.Schema;
 using System.Xml.XPath;
@@ -21,66 +22,121 @@ using System.Xml.XPath;
 string[] lines = File.ReadAllLines("input.txt");
 
 var l= lines.Length;
-var c= lines[0].Length;
-
-
-char[][] m = new char[l][];
-int splits = 0;
-long[][] cost = new long[l][];
-
-bool[][] solved = new bool[l][];
+var points = new List<Point>();
 for (int i = 0; i < l; i++)
 {
-    m[i] = lines[i].ToCharArray();
-    solved[i] = new bool[c];
-    cost[i]= new long[c];
-}
-int starti = 0;
-int startj = 0;
-for (int i = 0; i < l; i++)
-{
-    for (int j = 0; j < c; j++)
+    var x = lines[i].Split(',');
+    points.Add(new Point()
     {
-        if (m[i][j] == 'S')
+        X= int.Parse(x[0]),
+        Y = int.Parse(x[1]),
+        Z = int.Parse(x[2]),
+        pos = i
+    });
+}
+
+List<List<int>> lists = new List<List<int>>();
+var dist = new List<Dist>();
+
+for (int i = 0; i < l; i++)
+{
+    for (int j = i+1; j < l; j++)
+    {
+        dist.Add(new Dist(){P1 = points[i], P2 = points[j],Distance = GetDist(points[i], points[j])});
+    }
+}
+dist = dist.OrderBy(d => d.Distance).ToList();
+int indexOfDist = 0;
+
+for (int i = 0; i < 1000; i++)
+{
+    var cl = FindClosest();
+
+    bool found= false;
+    foreach (var lista in lists)
+    {
+        if (lista.Contains(cl.Item1.pos)&&!found)
         {
-            starti = i;
-            startj = j;
-            cost[i][j] = 1;
-            break;
+            if (!lista.Contains(cl.Item2.pos))
+            {
+                lista.Add(cl.Item2.pos);
+            }
+            found = true;
+        }
+        else if (lista.Contains(cl.Item2.pos)&&!found)
+        {
+            if (!lista.Contains(cl.Item1.pos))
+                lista.Add(cl.Item1.pos);
+            found = true;
         }
     }
-}
 
-for (int i = 0; i < l; i++)
-{
-    for (int j = 0; j < c; j++)
+    if (!found)
     {
-        if (cost[i][j] != 0)
+        var y = new List<int>() { cl.Item1.pos, cl.Item2.pos };
+        lists.Add(y);
+    }
+    else
+    {
+        //check if there is a common number in the queues, merge them
+        for (int j = 0; j < lists.Count; j++)
         {
-            PropCost(i, j);
+            for (int k = j+1; k < lists.Count; k++)
+            {
+                foreach (var number in lists[j])
+                {
+                    if (lists[k].Contains(number))
+                    {
+                        //merge
+                        lists[k].Remove(number);
+                        lists[j].AddRange(lists[k]);
 
-          
+                        lists.RemoveAt(k);
+                        break;
+                    }
+                }
+            }
         }
     }
+
 }
 
-long ss = 0;
-for (int i = 0; i < c; i++)
+
+
+lists.Sort((a, b) => a.Count.CompareTo(b.Count));
+lists.Reverse();
+Console.WriteLine(lists[0].Count * lists[1].Count*lists[2].Count);
+
+
+(Point,Point) FindClosest()
 {
-    ss += cost[l - 1][i];
+    dist[indexOfDist].Taken = true;
+    var x= (dist[indexOfDist].P1, dist[indexOfDist].P2);
+    indexOfDist++;
+    return x;
 }
-Console.WriteLine(ss);
-void PropCost(int i, int j)
+
+double GetDist(Point point1, Point point2)
 {
-    if(i==l-1)
-        return;
-    if (m[i + 1][j]=='.')
-        cost[i+1][j] += cost[i][j];
-    if (m[i + 1][j] == '^')
-    {
-        if (j > 0)
-            cost[i + 1][j - 1] += cost[i][j];
-        if (j < c - 1)
-            cost[i + 1][j + 1] += cost[i][j];
-    }
+    long sum = (point1.X - point2.X) * (point1.X - point2.X) +
+                     (point1.Y - point2.Y) * (point1.Y - point2.Y)+
+                     (point1.Z - point2.Z) * (point1.Z - point2.Z);
+    return Math.Sqrt(sum);
+
+}
+
+class Point
+{
+    public long X { get; set; }
+    public long Y { get; set; }
+    public long Z { get; set; }
+    public int pos { get; set; }
+}
+
+class Dist
+{
+    public Point P1 { get; set; }
+    public Point P2 { get; set; }
+    public double Distance { get; set; }
+    public bool Taken { get; set; }
 }
