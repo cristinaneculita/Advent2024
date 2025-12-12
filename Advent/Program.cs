c7c6f2dd6ea826999c3d931ffd1eab6cd0c60056
Cristina Neculita <cristina.neculita@scania.com>
2025-12-12 15:08:47 +0100

    2025 9_2

--- Advent/Program.cs
+++ Advent/Program.cs
@@ -18,6 +18,7 @@ using System.Security.Cryptography;
 using System.Xml;
 using System.Xml.Schema;
 using System.Xml.XPath;
+ Dictionary<(long, long), bool> pointInsideCache = new Dictionary<(long, long), bool>();
 
 string[] lines = File.ReadAllLines("input.txt");
 
@@ -32,70 +33,70 @@ for (int i = 0; i < l; i++)
         Y = int.Parse(x[1])
     });
 }
-var intpoints = new List<Point>();
-for (int i = 0; i < points.Count-1; i++)
-{
-    Console.WriteLine("points "+i+" out of "+l);
-    if (points[i].X == points[i + 1].X)
-    {
-         long min = points[i].Y < points[i + 1].Y ? points[i].Y : points[i + 1].Y;
-         long max = points[i].Y > points[i + 1].Y ? points[i].Y : points[i + 1].Y;
-         for (long j = min+1; j < max; j++)
-         {
-             if(!intpoints.Any(p=>p.X==points[i].X && p.Y==j))
-                 intpoints.Add( new Point(){ X= points[i].X, Y=j});
-         }
-    }
-    if (points[i].Y == points[i + 1].Y)
-    {
-        long min = points[i].X < points[i + 1].X ? points[i].X : points[i + 1].X;
-        long max = points[i].X > points[i + 1].X ? points[i].X : points[i + 1].X;
-        for (long j = min + 1; j < max; j++)
-        {
-            if (!intpoints.Any(p => p.X ==j && p.Y == points[i].Y))
-                intpoints.Add(new Point() { X = j, Y = points[i].Y });
-        }
-    }
-}
+//var intpoints = new List<Point>();
+//for (int i = 0; i < points.Count-1; i++)
+//{
+//    Console.WriteLine("points "+i+" out of "+l);
+//    if (points[i].X == points[i + 1].X)
+//    {
+//         long min = points[i].Y < points[i + 1].Y ? points[i].Y : points[i + 1].Y;
+//         long max = points[i].Y > points[i + 1].Y ? points[i].Y : points[i + 1].Y;
+//         for (long j = min+1; j < max; j++)
+//         {
+//             if(!intpoints.Any(p=>p.X==points[i].X && p.Y==j))
+//                 intpoints.Add( new Point(){ X= points[i].X, Y=j});
+//         }
+//    }
+//    if (points[i].Y == points[i + 1].Y)
+//    {
+//        long min = points[i].X < points[i + 1].X ? points[i].X : points[i + 1].X;
+//        long max = points[i].X > points[i + 1].X ? points[i].X : points[i + 1].X;
+//        for (long j = min + 1; j < max; j++)
+//        {
+//            if (!intpoints.Any(p => p.X ==j && p.Y == points[i].Y))
+//                intpoints.Add(new Point() { X = j, Y = points[i].Y });
+//        }
+//    }
+//}
 
-if (points[0].X == points[l-1].X)
-{
-    long min = points[0].Y < points[l-1].Y ? points[0].Y : points[l-1].Y;
-    long max = points[0].Y > points[l - 1].Y ? points[0].Y : points[l - 1].Y;
-    for (long j = min + 1; j < max; j++)
-    {
-        if (!intpoints.Any(p => p.X == points[0].X && p.Y == j))
-            intpoints.Add(new Point() { X = points[0].X, Y = j });
-    }
-}
-if (points[0].Y == points[l - 1].Y)
-{
-    long min = points[0].X < points[l - 1].X ? points[0].X : points[l - 1].X;
-    long max = points[0].X > points[l - 1].X ? points[0].X : points[l - 1].X;
-    for (long j = min + 1; j < max; j++)
-    {
-        if (!intpoints.Any(p => p.X == j && p.Y == points[0].Y))
-            intpoints.Add(new Point() { X = j, Y = points[0].X });
-    }
-}
+//if (points[0].X == points[l-1].X)
+//{
+//    long min = points[0].Y < points[l-1].Y ? points[0].Y : points[l-1].Y;
+//    long max = points[0].Y > points[l - 1].Y ? points[0].Y : points[l - 1].Y;
+//    for (long j = min + 1; j < max; j++)
+//    {
+//        if (!intpoints.Any(p => p.X == points[0].X && p.Y == j))
+//            intpoints.Add(new Point() { X = points[0].X, Y = j });
+//    }
+//}
+//if (points[0].Y == points[l - 1].Y)
+//{
+//    long min = points[0].X < points[l - 1].X ? points[0].X : points[l - 1].X;
+//    long max = points[0].X > points[l - 1].X ? points[0].X : points[l - 1].X;
+//    for (long j = min + 1; j < max; j++)
+//    {
+//        if (!intpoints.Any(p => p.X == j && p.Y == points[0].Y))
+//            intpoints.Add(new Point() { X = j, Y = points[0].Y });
+//    }
+//}
 
 // All contour points include both original points and interpolated points
 var allContourPoints = new HashSet<(long, long)>();
 var edges = new HashSet<(long, long)>();
 var lns = new HashSet<(long, long)>();
 // Add all original points
-foreach (var p in points)
-{
-    allContourPoints.Add((p.X, p.Y));
-    edges.Add((p.X, p.Y));
-}
+//foreach (var p in points)
+//{
+//    allContourPoints.Add((p.X, p.Y));
+//    edges.Add((p.X, p.Y));
+//}
 
-// Add all interpolated points
-foreach (var p in intpoints)
-{
-    allContourPoints.Add((p.X, p.Y));
-    lns.Add((p.X, p.Y));
-}
+//// Add all interpolated points
+//foreach (var p in intpoints)
+//{
+//    allContourPoints.Add((p.X, p.Y));
+//    lns.Add((p.X, p.Y));
+//}
 
 
 
@@ -124,131 +125,133 @@ bool AllPointsInContour(long x1, long y1, long x2, long y2)
     var maxx = x1 > x2 ? x1 : x2;
     var miny = y1 < y2 ? y1 : y2;
     var maxy = y1 > y2 ? y1 : y2;
+
+    if (!IsPointInsideContour(minx, miny, points) ||
+        !IsPointInsideContour(maxx, miny, points) ||
+        !IsPointInsideContour(minx, maxy, points) ||
+        !IsPointInsideContour(maxx, maxy, points))
+        return false;
+
+    // Check edges more sparsely
+    long stepSize = Math.Max(1, (maxx - minx) / 10); // Sample every 10th point
+
+    for (long x = minx; x <= maxx; x += stepSize)
+    {
+        if (!IsPointInsideContour(x, miny, points) ||
+            !IsPointInsideContour(x, maxy, points))
+            return false;
+    }
+
+    for (long y = miny; y <= maxy; y += stepSize)
+    {
+        if (!IsPointInsideContour(minx, y, points) ||
+            !IsPointInsideContour(maxx, y, points))
+            return false;
+    }
+
     for (long i = minx; i <= maxx; i++)
     {
         for (long j = miny; j <= maxy; j++)
         {
-            if (!IsPointInsideContour(i, j, points, intpoints))
+            if (!IsPointInsideContour(i, j, points))
                 return false;
         }
     }
 
     return true;
 }
- bool IsPointInsideContour(long x, long y, List<Point> points, List<Point> intpoints)
+bool IsPointInsideContour(long x, long y, List<Point> points)
 {
+    // Check cache first
+    var cacheKey = (x, y);
+    if (pointInsideCache.TryGetValue(cacheKey, out bool cachedResult))
+    {
+        return cachedResult;
+    }
 
-    // If point is exactly on the contour, it's inside
-    if (allContourPoints.Contains((x, y)))
+    // Check if point is exactly on a vertex
+    if (points.Any(p => p.X == x && p.Y == y))
+    {
+        pointInsideCache[cacheKey] = true;
         return true;
+    }
 
-    // Count contour points to the left, but handle edge cases
-    var leftCount = 0;
-
-    // Count original points (edges) to the left on the same Y line
-    var edgesOnLineToLeft = points.Where(p => p.Y == y && p.X < x).OrderBy(p => p.X).ToList();
-
-    // Process each edge point to determine if we should count it
-    foreach (var edgePoint in edgesOnLineToLeft)
+    // Check if point is on an edge
+    for (int i = 0; i < points.Count; i++)
     {
-        // Find the index of this edge point in the original points list
-        var edgeIndex = points.FindIndex(p => p.X == edgePoint.X && p.Y == edgePoint.Y);
+        var p1 = points[i];
+        var p2 = points[(i + 1) % points.Count];
 
-        if (edgeIndex != -1)
+        // Check if point is on this edge
+        if (IsPointOnEdge(x, y, p1.X, p1.Y, p2.X, p2.Y))
         {
-            // Get previous and next points to determine line directions
-            var prevPoint = points[(edgeIndex - 1 + points.Count) % points.Count];
-            var nextPoint = points[(edgeIndex + 1) % points.Count];
+            pointInsideCache[cacheKey] = true;
+            return true;
+        }
+    }
 
-            // Determine directions of lines coming from this edge
-            var prevDirection = prevPoint.Y > y ? 1 : (prevPoint.Y < y ? -1 : 0);
-            var nextDirection = nextPoint.Y > y ? 1 : (nextPoint.Y < y ? -1 : 0);
+    // Ray casting algorithm with proper handling of edge cases
+    int intersections = 0;
 
-            // If both directions are the same (both up or both down), skip this edge
-            if (prevDirection == nextDirection && prevDirection != 0)
+    for (int i = 0; i < points.Count; i++)
+    {
+        var p1 = points[i];
+        var p2 = points[(i + 1) % points.Count];
+
+        // Skip horizontal edges (they don't contribute to crossings)
+        if (p1.Y == p2.Y)
+            continue;
+
+        // Ensure p1.Y < p2.Y for consistent processing
+        if (p1.Y > p2.Y)
+        {
+            (p1, p2) = (p2, p1); // Swap
+        }
+
+        // Check if ray intersects this edge
+        // Ray must be strictly between p1.Y and p2.Y (not touching endpoints)
+        if (y > p1.Y && y <= p2.Y)
+        {
+            // Calculate intersection point
+            long intersectX = p1.X + ((y - p1.Y) * (p2.X - p1.X)) / (p2.Y - p1.Y);
+
+            // Count intersection if it's to the right of our point
+            if (x < intersectX)
             {
-                // Same direction - don't count this edge
-                continue;
-            }
-            // If directions are opposite or one is horizontal, count as 1
-            else
-            {
-                leftCount += 1;
+                intersections++;
             }
         }
     }
 
-    // Count interpolated points to the left (these are always counted normally)
-    var interpolatedToLeft = intpoints.Where(p => p.Y == y && p.X < x);
-
-    // Group consecutive interpolated points and handle them as line segments
-    var groupedInterpolated = new List<List<Point>>();
-    var currentGroup = new List<Point>();
-
-    foreach (var point in interpolatedToLeft.OrderBy(p => p.X))
-    {
-        if (currentGroup.Count == 0 || point.X == currentGroup.Last().X + 1)
-        {
-            currentGroup.Add(point);
-        }
-        else
-        {
-            groupedInterpolated.Add(currentGroup);
-            currentGroup = new List<Point> { point };
-        }
-    }
-    if (currentGroup.Count > 0)
-        groupedInterpolated.Add(currentGroup);
-
-    // For each group of consecutive interpolated points, check edge directions
-    foreach (var group in groupedInterpolated)
-    {
-        var startX = group.First().X;
-        var endX = group.Last().X;
-
-        // Find the edges that bound this line segment
-        var leftEdge = points.Where(p => p.X < startX && p.Y == y).OrderByDescending(p => p.X).FirstOrDefault();
-        var rightEdge = points.Where(p => p.X > endX && p.Y == y).OrderBy(p => p.X).FirstOrDefault();
-
-        if (leftEdge != null && rightEdge != null)
-        {
-            // Get directions from both edges
-            var leftEdgeIndex = points.FindIndex(p => p.X == leftEdge.X && p.Y == leftEdge.Y);
-            var rightEdgeIndex = points.FindIndex(p => p.X == rightEdge.X && p.Y == rightEdge.Y);
-
-            if (leftEdgeIndex != -1 && rightEdgeIndex != -1)
-            {
-                var leftPrev = points[(leftEdgeIndex - 1 + points.Count) % points.Count];
-                var leftNext = points[(leftEdgeIndex + 1) % points.Count];
-                var rightPrev = points[(rightEdgeIndex - 1 + points.Count) % points.Count];
-                var rightNext = points[(rightEdgeIndex + 1) % points.Count];
-
-                var leftOutDirection = leftNext.Y > y ? 1 : (leftNext.Y < y ? -1 : 0);
-                var rightOutDirection = rightPrev.Y > y ? 1 : (rightPrev.Y < y ? -1 : 0);
-
-                if (leftOutDirection == rightOutDirection && leftOutDirection != 0)
-                {
-                    // Same direction - don't count any points in this line segment
-                    continue;
-                }
-                else
-                {
-                    // Opposite directions - count as 1 regardless of how many points
-                    leftCount += 1;
-                }
-            }
-        }
-        else
-        {
-            // No bounding edges found, count all points normally
-            leftCount += group.Count;
-        }
-    }
-
-    // If odd number of effective crossings, point is inside
-    return leftCount % 2 == 1;
+    bool result = (intersections % 2) == 1;
+    pointInsideCache[cacheKey] = result;
+    return result;
 }
 
+// Helper method to check if point is on edge
+bool IsPointOnEdge(long px, long py, long x1, long y1, long x2, long y2)
+{
+    // Check if point is on line segment
+    if (x1 == x2) // Vertical line
+    {
+        return px == x1 && py >= Math.Min(y1, y2) && py <= Math.Max(y1, y2);
+    }
+    else if (y1 == y2) // Horizontal line  
+    {
+        return py == y1 && px >= Math.Min(x1, x2) && px <= Math.Max(x1, x2);
+    }
+    else
+    {
+        // General line - check if point lies on line segment
+        // Using cross product to check collinearity and range check
+        long crossProduct = (py - y1) * (x2 - x1) - (px - x1) * (y2 - y1);
+        if (crossProduct != 0) return false; // Not collinear
+
+        // Check if point is within segment bounds
+        return px >= Math.Min(x1, x2) && px <= Math.Max(x1, x2) &&
+               py >= Math.Min(y1, y2) && py <= Math.Max(y1, y2);
+    }
+}
 class Point
 {
     public long X { get; set; }
